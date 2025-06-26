504c9d0351153da5e07a4dafb08906bc4f1cf109
ChristianPE1 <cpardave@unsa.edu.pe>
2025-06-25 23:40:38 -0500

    Add: logs to encoder

--- src/transformer/encoder.cuh
+++ src/transformer/encoder.cuh
@@ -21,10 +21,52 @@ public:
           norm2(d_model) {}
 
     Matrix forward(const Matrix &input, const Matrix *src_mask = nullptr) {
+        printf("[ENCODER_LAYER] Starting layer with input stats: ");
+        // Quick debug of input
+        std::vector<float> debug_input(std::min(10, (int)(input.getRows() * input.getCols())));
+        input.copyToHost(debug_input);
+        int non_zero = 0;
+        for (float val : debug_input) if (val != 0.0f) non_zero++;
+        printf("%d/%d non-zero\n", non_zero, (int)debug_input.size());
+        
         Matrix self_att_output = self_attention.forward(input, input, input, src_mask ? *src_mask : Matrix());
-        Matrix norm1_output = norm1.forward(input.add(self_att_output));
+        
+        printf("[ENCODER_LAYER] After self-attention: ");
+        std::vector<float> debug_att(std::min(10, (int)(self_att_output.getRows() * self_att_output.getCols())));
+        self_att_output.copyToHost(debug_att);
+        non_zero = 0;
+        for (float val : debug_att) if (val != 0.0f) non_zero++;
+        printf("%d/%d non-zero\n", non_zero, (int)debug_att.size());
+        
+        Matrix residual1 = input.add(self_att_output);
+        Matrix norm1_output = norm1.forward(residual1);
+        
+        printf("[ENCODER_LAYER] After norm1: ");
+        std::vector<float> debug_norm1(std::min(10, (int)(norm1_output.getRows() * norm1_output.getCols())));
+        norm1_output.copyToHost(debug_norm1);
+        non_zero = 0;
+        for (float val : debug_norm1) if (val != 0.0f) non_zero++;
+        printf("%d/%d non-zero\n", non_zero, (int)debug_norm1.size());
+        
         Matrix ff_output = feed_forward.forward(norm1_output);
-        Matrix norm2_output = norm2.forward(norm1_output.add(ff_output));
+        
+        printf("[ENCODER_LAYER] After feedforward: ");
+        std::vector<float> debug_ff(std::min(10, (int)(ff_output.getRows() * ff_output.getCols())));
+        ff_output.copyToHost(debug_ff);
+        non_zero = 0;
+        for (float val : debug_ff) if (val != 0.0f) non_zero++;
+        printf("%d/%d non-zero\n", non_zero, (int)debug_ff.size());
+        
+        Matrix residual2 = norm1_output.add(ff_output);
+        Matrix norm2_output = norm2.forward(residual2);
+        
+        printf("[ENCODER_LAYER] Final output: ");
+        std::vector<float> debug_final(std::min(10, (int)(norm2_output.getRows() * norm2_output.getCols())));
+        norm2_output.copyToHost(debug_final);
+        non_zero = 0;
+        for (float val : debug_final) if (val != 0.0f) non_zero++;
+        printf("%d/%d non-zero\n", non_zero, (int)debug_final.size());
+        
         return norm2_output;
     }
 };
